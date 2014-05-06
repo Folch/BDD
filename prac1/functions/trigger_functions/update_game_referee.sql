6521d0cc57f2782d1c798a8b4d563d9494225fef
Folch <albertfolch58@gmail.com>
2014-05-06 20:12:02 +0200

    olakase

--- prac1/functions/trigger_functions/update_game_referee.sql
+++ prac1/functions/trigger_functions/update_game_referee.sql
@@ -2,24 +2,23 @@ CREATE OR REPLACE FUNCTION update_game_referee() RETURNS TRIGGER AS $$
 	DECLARE
 		dniWinner text;
 		dniBlanc text;
-		jutgeTmp text;
 	BEGIN
-		--IF NOT FOUND THEN 
-		dniWinner = (SELECT victoria FROM partida);
-		dniBlanc = (SELECT jblanques FROM partida);
+
+		dniWinner = NEW.victoria;
+		dniBlanc = NEW.jblanques;
 
 		IF (dniWinner like dniBlanc) THEN
 			IF (NEW.victoria is not null) then
 				update jutge
-				SET pblanques =+ 1
-				where persona like jutge.persona;
+				SET pblanques = pblanques + 1
+				where new.jutge = jutge.persona;
 			END IF;
 		ELSE
 
 			IF (NEW.victoria is not null) then
 				update jutge
-				SET pnegres =+ 1
-				where persona like jutge.persona;
+				SET pnegres =pnegres + 1
+				where new.jutge = jutge.persona;
 			END IF;
 		END IF;
 		RETURN NULL;
