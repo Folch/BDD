d0f5a85688ed99fe090b321e443525db94a662f2
Folch <albertfolch58@hotmail.com>
2014-05-21 16:17:44 +0200

    u

--- prac1/A1_Folch_Moreno/src/create/create_views.sql
+++ prac1/A1_Folch_Moreno/src/create/create_views.sql
@@ -1,6 +1,6 @@
 CREATE VIEW v_taquillers AS
 
-SELECT h.nom as Hotel,h.direccio,s.nom as Sala,(s.aforament - e.entradesvenudes) as Entrades_disponibles, j.id AS jornada,j.datainici,j.datafi,p.jblanques AS Jugador_blanques,p.jnegres AS Jugador_negres,p.victoria,p.jutge
+SELECT h.nom as Hotel,h.direccio,s.nom as Sala,(s.aforament - e.entradesvenudes) as Entrades_disponibles, j.id AS jornada,j.datainici,j.datafi,p.id as Id_partida,p.jblanques AS Jugador_blanques,p.jnegres AS Jugador_negres,p.victoria,p.jutge
 FROM hotel h INNER JOIN sala s ON (h.nom = s.hotel) 
 INNER JOIN entrada e ON (e.sala = s.nom AND e.hotel = h.nom) 
 INNER JOIN jornada j ON (e.jornada = j.id) 
